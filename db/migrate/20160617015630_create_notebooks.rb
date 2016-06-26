d562cfbb19ad10fdf000a43be846ff6878118299
ponkotsur <ponkotsur@gmail.com>
2016-06-26 12:32:40 +0900

    Revert "migrate update"
    This reverts commit 92b2c237ed406ef0e19cddefb790c462a068f27d.

--- db/migrate/20160617015630_create_notebooks.rb
+++ db/migrate/20160617015630_create_notebooks.rb
@@ -1,10 +1,10 @@
 class CreateNotebooks < ActiveRecord::Migration
   def change
     create_table :notebooks do |t|
-      t.string :guid
       t.string :title
-      t.string :published
+      t.integer :guid
 
+      t.timestamps null: false
     end
   end
 end
